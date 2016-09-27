cde3bf68ed88242e33c816feda908c7b9e020617
Nicolas Corrarello <ncorrare@gmail.com>
2016-09-27 10:46:14 +0100

    First attempt at retrieving info

--- app.rb
+++ app.rb
@@ -10,6 +10,12 @@ get '/' do
 end
 get '/participants/:username' do
 	#Display user information. Query PuppetDB for the Azure object for that particular user
+        yaml_string = File.read 'users.yaml'
+	data = YAML.load yaml_string
+	username = params['username']
+	user = data.fetch("users").select {|s| s.include? username}
+	fullname = user[0].fetch(username).fetch("fullname")
+	"#{fullname}"
 end
 post '/participant' do
 	#Create an Azure VM, console user, node group, blah. If succesfull, redirect to get '/participants/:username'
@@ -20,5 +26,6 @@ post '/participant' do
 	data["users"] << { username => new_user }
 	output = YAML.dump data
 	File.write('users.yaml', output)
+	redirect to("/participants/#{username}")
 end
 
