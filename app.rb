034fe6ea0dba8b7ff7b62814e23ed986d5790c2b
Nicolas Corrarello <ncorrare@gmail.com>
2016-10-03 13:41:28 +0100

    Adding the username to the hash

--- app.rb
+++ app.rb
@@ -24,8 +24,8 @@ post '/participant' do
 	#Create an Azure VM, console user, node group, blah. If succesfull, redirect to get '/participants/:username'
 	yaml_string = File.read 'users.yaml'
 	data = YAML.load yaml_string
-	new_user = { 'fullname' => params['fullname'], 'email' => params['email'], 'password' => params['password']}
 	username = params['username']
+	new_user = { 'username' => username, 'fullname' => params['fullname'], 'email' => params['email'], 'password' => params['password']}
 	data["users"] << { username => new_user }
 	output = YAML.dump data
 	File.write('users.yaml', output)
